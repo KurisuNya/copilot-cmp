7689d4019b12db1cc50922ac40fb0d447874d4f7
KurisuNya <cuizhu1216@gmail.com>
2023-11-26 15:36:15 +0800

    feat: add text after cursor to the end of copilot text

--- lua/copilot_cmp/source.lua
+++ lua/copilot_cmp/source.lua
@@ -3,15 +3,15 @@ local source = {
 }
 
 function source:get_keyword_pattern()
-  return '.'
+  return "."
 end
 
 source.get_trigger_characters = function()
-  return {'.'}
+  return { "." }
 end
 
 -- executes before selection
-source.resolve = function (self, completion_item, callback)
+source.resolve = function(self, completion_item, callback)
   for _, fn in ipairs(self.executions) do
     completion_item = fn(completion_item)
   end
@@ -37,20 +37,21 @@ source.is_available = function(self)
   return true
 end
 
-source.execute = function (self, completion_item, callback)
+---@diagnostic disable-next-line: unused-local
+source.execute = function(self, completion_item, callback)
   callback(completion_item)
 end
 
-source.new = function(client, opts)
+source.new = function(client)
   local completion_functions = require("copilot_cmp.completion_functions")
 
   local self = setmetatable({
-    timer = vim.loop.new_timer()
+    timer = vim.loop.new_timer(),
   }, { __index = source })
 
   self.client = client
   self.request_ids = {}
-  self.complete = completion_functions.init('getCompletionsCycling', opts)
+  self.complete = completion_functions.init("getCompletionsCycling")
 
   return self
 end
