41fd168cfc7a67ff5f14278df179a4a344de2bec
KurisuNya <cuizhu1216@gmail.com>
2024-05-13 02:59:53 +0800

    feat: small optimize

--- lua/copilot_cmp/pattern.lua
+++ lua/copilot_cmp/pattern.lua
@@ -1,30 +1,15 @@
 local pattern = {}
 
-local chars = {
-  ["("] = "(",
-  [")"] = ")",
-  ["["] = "[",
-  ["]"] = "]",
-  ["{"] = "{",
-  ["}"] = "}",
-}
-
 local fmt_chars = {
   ["("] = "%(",
   ["["] = "%[",
   ["{"] = "%{",
-  [")"] = "%)",
-  ["]"] = "%]",
-  ["}"] = "%}",
 }
 
 local char_pairs = {
   ["("] = "%)",
   ["["] = "%]",
   ["{"] = "%}",
-  [")"] = "%(",
-  ["]"] = "%[",
-  ["}"] = "%{",
 }
 
 local function count_char(str, char)
@@ -32,8 +17,8 @@ local function count_char(str, char)
 end
 
 pattern.check_pairs = function(text)
-  for _, char in pairs(chars) do
-    local char_count = count_char(text, fmt_chars[char])
+  for char, fmt_char in pairs(fmt_chars) do
+    local char_count = count_char(text, fmt_char)
     local pair_count = count_char(text, char_pairs[char])
     if char_count ~= pair_count then
       return false
